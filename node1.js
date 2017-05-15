6dc28c322bc04b5de3a4754154f7bbfab95ad536
Vatspra <prashant7196@gmail.com>
2017-05-16 00:25:35 +0530

    i added a basic http file

---
+++ node1.js
@@ -0,0 +1,16 @@
+
+var http = require('http');
+var fs = require('fs');
+
+//var readStream = fs.createReadStream(__dirname + '/text2.txt','utf8');
+//var writeStream = fs.createWriteStream(__dirname +'/write1.txt');
+//readStream.pipe(writeStream);(
+var server = http.createServer(function(req,res){
+	console.log('request was made at :' +req.url);
+	res.writeHead(200,{'Content-Type': 'text/plain'});
+	var readStream = fs.createReadStream(__dirname + '/text2.txt','utf8');
+	readStream.pipe(res)
+
+});
+server.listen(3000,'127.0.0.1');
+console.log('we are listening at port 3000');
